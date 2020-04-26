fa53825da1b73584a96b3c9230effde51907634c
Angel Anaya <alex.eepsilon@gmail.com>
2020-04-25 22:42:55 -0500

    Style landing screen

--- src/modules/Landing/LandingScreen.tsx
+++ src/modules/Landing/LandingScreen.tsx
@@ -1,8 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+
+import { Text } from "../../components";
+import colors from "../../colors";
+import { ServiceSelect, IService } from "./components";
+
+const services: IService[] = [
+  {
+    id: "1",
+    name: "DRINKING_WATER",
+    displayName: "Servicio de agua"
+  },
+  {
+    id: "2",
+    name: "GAS",
+    displayName: "Servicio de gas"
+  }
+]
 
 export const LandingScreen: React.FC = () => (
-  <View>
+  <SafeAreaView style={styles.container}>
     <Text>LandingScreen</Text>
-  </View>
+    <ServiceSelect services={services} />
+  </SafeAreaView>
 );
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    borderTopWidth: 2,
+    borderTopColor: colors.brandOrange,
+    backgroundColor: colors.tintedWhite
+  }
+});
+
+(LandingScreen as any).options = {
+  topBar: {
+    title: {
+      text: "Hello"
+    }
+  }
+};
