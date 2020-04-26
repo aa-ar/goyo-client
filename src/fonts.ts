f3eb0ceb626c8cc04da31a0d595dc02caf4151c2
Angel Anaya <alex.eepsilon@gmail.com>
2020-04-25 21:58:38 -0500

    Add fonts object

---
+++ src/fonts.ts
@@ -0,0 +1,13 @@
+export default {
+  Inter: {
+    Thin: "Inter-Thin",
+    ExtraLight: "Inter-ExtraLight",
+    Light: "Inter-Light",
+    Regular: "Inter-Regular",
+    Medium: "Inter-Medium",
+    SemiBold: "Inter-SemiBold",
+    Bold: "Inter-Bold",
+    ExtraBold: "Inter-ExtraBold",
+    Black: "Inter-Black"
+  }
+}
